2700dad4fa03c715bc1599f70b037aadbcb163c1
ElenAlmeida <elenalmeida@outlook.com>
2019-05-06 17:37:46 -0300

    Função para validar cartão finalizada.

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,11 +6,17 @@ describe('cardValidator', () =>{
     expect(Index.cardValidator('0123456711')).to.equal(false);          
   });
   it('should return false if it is less than 12 or greater than 19 digits', () =>{
-    expect(Index.cardValidator('123456789123456789123')).to.equal(false);          
+    expect(Index.cardValidator('123456789123456789112354')).to.equal(false);          
   });
   it('should return false for information other than number', () =>{
-    expect(Index.cardValidator('asdfghjklçpoiuyt')).to.equal(false);          
+    expect(Index.cardValidator('AFDGTHRUEILGJOK')).to.equal(false);          
   });
+  it('should return false if the last digit is not equal', () =>{
+    expect(Index.cardValidator('1234567891234567')).to.equal(false);          
+  }); 
+  it('should return false if the last digit is not equal', () =>{
+    expect(Index.cardValidator('1234567891234567')).to.equal(false);          
+  });     
   it('should return false for equal numbers', () => {
     expect(Index.cardValidator('0000000000000000')).to.equal(false);
   });   
